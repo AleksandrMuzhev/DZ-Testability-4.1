67761c602eb2614aa03e53bdf252395c0ca4dea7
Aleksandr <126883049+AleksandrMuzhev@users.noreply.github.com>
2023-04-07 12:45:09 +0500

    Update BonusMilesService.java

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -9,23 +9,3 @@ public class BonusMilesService {
         return result;
     }
 }
-
-
-
-
-
-
-
-/*public class Main {
-    public static void main(String[] args) {
-        int amount = 13676; //стоимость билета
-        int bonus = 20; //количество рублей для одной бонусной мили
-
-        int accrual = amount / bonus; //accrual - количество начисленных бонусных миль
-        if (amount >= 20) {
-            System.out.println(accrual);
-        } else {
-            System.out.println("Бонусные мили = 0");
-        }
-    }
-}*/
